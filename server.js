5a0c09fa6619c03531cad2a97e805aec79b16258
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2026-03-10 19:27:37 -0500

    creando los usuarios simulados

--- server.js
+++ server.js
@@ -6,6 +6,25 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 
+const usuarios = [
+  {
+    id: 1,
+    email: "juancarlospabon01@gmail.com",
+    password: "123",
+    tol: "admin",
+    code2fa: null,
+    refreshToken: null,
+  },
+  {
+    id: 2,
+    email: "munozpabonjuancarlos2@gmail.com",
+    password: "123",
+    rol: "estudiante",
+    code2fa: null,
+    refreshToken: null,
+  },
+];
+
 app.get("/login", (req, res) => {
   console.log("Inicio de sesión exitoso.");
 });
