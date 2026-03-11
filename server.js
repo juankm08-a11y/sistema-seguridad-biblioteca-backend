f3900a2d3d5d3ff1d87eba9fcb91864df96fb550
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2026-03-10 19:46:19 -0500

    arreglando bug de cors

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ const { verificarToken, permitirRoles } = require("./authMiddleware");
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
+app.options("*", cors());
 const usuarios = [
   {
     id: 1,
